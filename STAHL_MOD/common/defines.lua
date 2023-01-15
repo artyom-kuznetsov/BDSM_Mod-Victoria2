59782578d6aad188ab74386a51e9c325761384fa
artyom-kuznetsov <olegtiger020@gmail.com>
2023-01-15 21:59:55 +0200

    BDSM v3.2.2P
    -Mil increase after rebel rising
    -Nerf assim from laws
    -Fix india crash
    -Spain gets cores on HAI, CUB and CLM/VNZ parts
    -Added Search for Machu Picchu archaeology
    -SPA monarchy flag
    -Removed torpedo attack

--- STAHL_MOD/common/defines.lua
+++ STAHL_MOD/common/defines.lua
@@ -666,7 +666,7 @@ pops = {
 	MIL_TO_JOIN_REBEL = 7, -- Rebels over this will join a faction
 	MIL_TO_JOIN_RISING = 8, -- Rebels over this will join a general rising
 	MIL_TO_AUTORISE = 9, -- Rebels over this rise no matter what
-	REDUCTION_AFTER_RISEING = 0.0, -- After a pop spawns a rebellion, its militancy will be reduced this much
+	REDUCTION_AFTER_RISEING = -0.5, -- After a pop spawns a rebellion, its militancy will be reduced this much
 	REDUCTION_AFTER_DEFEAT = 3.0, -- After a rebellion is being defeated in combat, its pop militancy will be divided by this number.
 																-- (if value < 1.0, the MIL will be increased) (Beware! value must be > 0)
 	
