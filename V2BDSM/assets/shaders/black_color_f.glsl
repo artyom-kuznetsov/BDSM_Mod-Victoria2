ef80b9c7f09855c6da059c9eda14baad5d687768
artyom-kuznetsov <olegtiger020@gmail.com>
2024-03-29 14:37:06 +0200

    new alice for debug

--- V2BDSM/assets/shaders/black_color_f.glsl
+++
@@ -1,5 +0,0 @@
-layout (location = 0) out vec4 frag_color;
-
-void main() {
-	frag_color = vec4(0.85, 0.85, 0.85, 1.);
-}
--- V2BDSM/assets/shaders/line_border_f.glsl
+++
@@ -1,6 +0,0 @@
-in vec2 tex_coord;
-layout (location = 0) out vec4 frag_color;
-
-void main() {
-	frag_color = vec4(0., 0., 0., 1.);
-}
