fbc062a2d5529b321d4ff11406e5988fb34ae3c3
artyom-kuznetsov <olegtiger020@gmail.com>
2024-03-31 14:50:49 +0300

    Novgorod, Voronezh, Caucasus, TUR Wars, EGY

--- V2BDSM/mod/2/common/defines.lua
+++ V2BDSM/mod/2/common/defines.lua
@@ -518,7 +518,7 @@ diplomacy = {
 	WARSUBSIDIES_PERCENT = 0.20, -- How many percent of imports you are going to pay each day(mil. constructions and mil. maintainence)
 	NEIGHBOUR_BONUS_INFLUENCE_PERCENT = 0.50, -- Bonus to neighbouring countries when influencing
 	SPHERE_NEIGHBOUR_BONUS_INFLUENCE_PERCENT = 0.2, -- Bonus to countries in your sphere neighbouring when influencing
-	OTHER_CONTINENT_BONUS_INFLUENCE_PERCENT = -0.50, -- Bonus to neighbouring countries when influencing
+	OTHER_CONTINENT_BONUS_INFLUENCE_PERCENT = -0.33, -- Bonus to neighbouring countries when influencing
 	PUPPET_BONUS_INFLUENCE_PERCENT = 1.0, -- bonus if they are our puppet
 	
 	-- effects of manually releasing a nation
