5751f2a98784e0dd250eab36c76f02b85c28756a
artyom-kuznetsov <olegtiger020@gmail.com>
2024-07-12 01:19:47 +0300

    Austria Subjugate SGF, Stability Updates, Rebel Rework #2, War Border Closure

--- V2BDSM/mod/2/common/defines.lua
+++ V2BDSM/mod/2/common/defines.lua
@@ -636,14 +636,14 @@ pops = {
 	LIFE_RATING_GROWTH_BONUS = 0.0001,
 	LIFE_NEED_STARVATION_LIMIT = 0.5,
 
-	MIL_LACK_EVERYDAY_NEED = 0.1,
+	MIL_LACK_EVERYDAY_NEED = 0.05,
 	MIL_HAS_EVERYDAY_NEED = -0.1,
 	MIL_HAS_LUXURY_NEED = -0.2,
 	MIL_NO_LIFE_NEED = 0.2,
 	MIL_REQUIRE_REFORM = 0.2,
 	MIL_IDEOLOGY = -0.1,
 	MIL_RULING_PARTY = -0.1,
-	MIL_REFORM_IMPACT = 0.5,
+	MIL_REFORM_IMPACT = 2,
 	MIL_WAR_EXHAUSTION = 0.005,
 	MIL_NON_ACCEPTED = 0.05,
 		
@@ -663,11 +663,11 @@ pops = {
 	POP_SAVINGS = 0.018,
 	
 	STATE_CREATION_ADMIN_LIMIT = 0.01,
-	MIL_TO_JOIN_REBEL = 7, -- Rebels over this will join a faction
-	MIL_TO_JOIN_RISING = 8, -- Rebels over this will join a general rising
+	MIL_TO_JOIN_REBEL = 8, -- Rebels over this will join a faction
+	MIL_TO_JOIN_RISING = 9, -- Rebels over this will join a general rising
 	MIL_TO_AUTORISE = 9, -- Rebels over this rise no matter what
 	REDUCTION_AFTER_RISEING = 0.0, -- After a pop spawns a rebellion, its militancy will be reduced this much
-	REDUCTION_AFTER_DEFEAT = 3.0, -- After a rebellion is being defeated in combat, its pop militancy will be divided by this number.
+	REDUCTION_AFTER_DEFEAT = 7.0, -- After a rebellion is being defeated in combat, its pop militancy will be divided by this number.
 																-- (if value < 1.0, the MIL will be increased) (Beware! value must be > 0)
 	
 	POP_TO_LEADERSHIP = 0.0001, -- how much leadership every 1000 officers gives each day.
